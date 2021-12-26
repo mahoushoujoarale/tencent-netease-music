885afcce38f7219c47f344d2d6da85047de4748e
monster <2536659921@qq.com>
2021-12-26 17:26:36 +0800

    先做到这儿

--- src/pages/User/components/UserSongList/UserSongList.tsx
+++ src/pages/User/components/UserSongList/UserSongList.tsx
@@ -1,42 +1,45 @@
-import React, {useEffect, useState} from "react";
-import './UserSongList.less'
+import React, { useEffect, useState } from "react";
+import "./UserSongList.less";
 import { getUserDetail } from "@/apis/user";
-import { useNavigate, useLocation,Link } from "react-router-dom";
+import { useNavigate, useLocation, Link } from "react-router-dom";
 import { getUserPlayList } from "@/apis/getUserPlayList";
-import { formatPlayCount, resetPlaylist} from "@/utils";
+import { formatPlayCount, resetPlaylist } from "@/utils";
 import { action } from "mobx";
 
-const UserSongList:React.FC = () => {
-    const navigate = useNavigate()
-    const location = useLocation()
-    const [userDetail,setUserDetail] = useState({
-        playlistCount:0
-    })
-    const [userPlayList, setUserPlayList] = useState([])
-    const uid = location.search.slice(4)
-    useEffect (()=> {
-        getUserDetail({id : uid}).then((res) => {
-            setUserDetail(res.profile)
-            //console.log("userDtail:",res)
-        })
-    },[])
+const UserSongList: React.FC = () => {
+  const navigate = useNavigate();
+  const location = useLocation();
+  const [userDetail, setUserDetail] = useState({
+    playlistCount: 0,
+  });
+  const [userPlayList, setUserPlayList] = useState([]);
+  const uid = location.search.slice(4);
+  useEffect(() => {
+    getUserDetail({ id: uid }).then((res) => {
+      setUserDetail(res.profile);
+      //console.log("userDtail:",res)
+    });
+  }, []);
 
-    useEffect (()=> {
-        getUserPlayList({id : uid}).then((res) => {
-            if (res.code === 200) {
-                setUserPlayList(res.playlist)
-            }
-            
-            //console.log("getUserPlayList:",res)
-        })
-    },[])
+  useEffect(() => {
+    getUserPlayList({ id: uid }).then((res) => {
+      if (res.code === 200) {
+        setUserPlayList(res.playlist);
+      }
 
-  return (<div>
+      //console.log("getUserPlayList:",res)
+    });
+  }, []);
+
+  return (
+    <div>
       <div className="songListHeader">
-          <p style={{fontSize: "22px", color: "#333"}}>我创建的歌单（{userDetail.playlistCount}）</p>
+        <p style={{ fontSize: "22px", color: "#333" }}>
+          我创建的歌单（{userDetail.playlistCount}）
+        </p>
       </div>
       <div className="songListContent">
-      { userPlayList.map(
+        {(userPlayList || []).map(
           (item: {
             id: string;
             coverImgUrl: string;
@@ -64,8 +67,7 @@ const UserSongList:React.FC = () => {
                             event.preventDefault();
                             resetPlaylist(item.id);
                           })}
-                        >
-                        </div>
+                        ></div>
                       </div>
                     </div>
                   </Link>
@@ -78,8 +80,8 @@ const UserSongList:React.FC = () => {
           }
         )}
       </div>
-  </div>
-  )
+    </div>
+  );
 };
 
 export default UserSongList;
